2ed70d6c6b2beb2b7b4c5b95e000aa569cc5acd4
kathrynzaro <zarokathryn@gmail.com>
2022-06-06 14:12:09 -0700

    adding header, footer, main, zodiac to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-{/* import the zodiac array */}
+import Header from './Header/Header.js';
+import Main from './Main/Main.js';
+import Footer from './Footer/Footer.js';
+import { zodiac } from './data.js';
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
+      <Header name='kat' />
       {/* the Footer component needs a year passed in as the `year` prop */}
+      <Footer year='2022' />
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
+      <Main zodiac={zodiac} />
     </div>
   );
 }
